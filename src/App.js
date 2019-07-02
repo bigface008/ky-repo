6ab5e430fd71ddd03f897831a0a00f4f5dcfd25c
bigface008 <bigface00@sjtu.edu.cn>
2019-07-02 16:00:06 +0800

    基本界面准备完毕

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, Card } from 'antd';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { output: '' };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('Button clicked.');
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Card className="App-Card" title="Output">
+          <p>{this.state.output}</p>
+        </Card>
+        <Button className="App-Button" type="primary" onClick={this.handleClick}>Calculate</Button>
       </div>
     );
   }
